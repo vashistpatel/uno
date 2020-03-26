0edb85aa9c7b5fe33026b1c31e191680e70db3b9
mayurbhai24 <55508847+mayurbhai24@users.noreply.github.com>
2020-03-26 15:55:29 -0400

    Updated FileIO file By Mayur

--- src/sample/FileIO.java
+++ src/sample/FileIO.java
@@ -1,72 +1,34 @@
 package sample;
 
-import javax.swing.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileIO {
     public static File filename_gameStats = new File("Game_Stats.txt");
     public static File filename_allStats = new File("All_Stats.txt");
 
-    public static void readFile (File filename) {
+    public static List<String> readFile (File filename) {
         try {
             if (filename.exists()) {
                 System.out.println("File Exists");
                 Scanner reader = new Scanner(filename);
-
+                List<String> data_array = new ArrayList<>();
                 while(reader.hasNextLine()) {
                     String data = reader.nextLine();
-                    System.out.println(data);
+                    data_array.add(data);
                 }
                 reader.close();
+                return data_array;
             } else { System.out.println("File Not Found!"); }
 
         } catch (IOException e) {
             System.out.println("Error occurred while reading file.");
             e.printStackTrace();
         }
+        return null;
     }
-
-//    public static void writeFile (File filename) {
-//        try {
-//            if (filename.exists()) {
-//                System.out.println("File Exists!");
-//                // This will append to the file. Use False to not append.
-//                if (filename.equals(filename_allStats)) {
-//                    FileWriter fileWriter = new FileWriter(filename, true);
-//                    PrintWriter printWriter = new PrintWriter(fileWriter);
-//                    System.out.println("It is allStats");
-//                } else if (filename.equals(filename_gameStats)) {
-//                    FileWriter fileWriter = new FileWriter(filename, true);
-//                    PrintWriter printWriter = new PrintWriter(fileWriter);
-//                    System.out.println("It is gamestats");
-//                }
-//
-//
-//                Statistics.computeStats();
-//                printWriter.println();
-//                printWriter.println();
-//                printWriter.println("Uno Game Statistics");
-//                printWriter.println("------------------------------------------");
-//                printWriter.println("+2 Cards Played: " + Statistics.plus_2_cards);
-//                printWriter.println("+4 Cards Played: " + Statistics.plus_4_cards);
-//                printWriter.println("Change Colour Cards Played: " + Statistics.change_cards);
-//                printWriter.println("Block Cards Played: " + Statistics.block_cards);
-//                printWriter.println("Reverse Cards Played: " + Statistics.reverse_cards);
-//                printWriter.println("Total Cards Played: " + Statistics.total_cards);
-//
-//                printWriter.close();
-//                fileWriter.close();
-//            } else {
-//                System.out.println("Creating New File!");
-//                filename.createNewFile();
-//            }
-//
-//        } catch (IOException e) {
-//            System.out.println("Error occurred while writing file.");
-//            e.printStackTrace();
-//        }
-//    }
 
     public static void writeFile () {
         FileWriter fileWriter = null;
@@ -117,8 +79,4 @@ public class FileIO {
             e.printStackTrace();
         }
     }
-
-
-
-
 }
