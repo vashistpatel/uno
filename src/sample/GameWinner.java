a415a98c4b027d949f9b6bf63c9cd2114a3a3966
vashist patel <vashist.patel@ontariotechu.net>
2020-12-30 19:44:10 -0500

    updated GameWinner popup screen

--- src/sample/GameWinner.java
+++ src/sample/GameWinner.java
@@ -3,8 +3,9 @@ package sample;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -18,9 +19,17 @@ public class GameWinner {
 
     public static void GameWinner2(boolean won){
         Pane winnerPane = new Pane();
+
+        Image img = new Image("CARDS/trophy.png",541.5254,700,false, false);
+        ImageView mv = new ImageView(img);
+//        mv.setFitHeight(450);
+//        mv.setFitWidth(348.1235);
+        BackgroundImage bImage = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT , BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        Background background = new Background(bImage);
+
         Stage stage2 = new Stage();
         stage2.setTitle("Winner Winner Chiken Dinner!");
-        stage2.setScene(new Scene(winnerPane, 450, 450));
+        stage2.setScene(new Scene(winnerPane, 541.5254, 700));
 
         Label p = new Label();
 
@@ -29,17 +38,18 @@ public class GameWinner {
             if(won == true){
                 System.out.println("player 1 won");
                 p.setText("player 1 won");
-                p.setTranslateX(50);
-                p.setTranslateY(250);
+                p.setTranslateX(205);
+                p.setTranslateY(590);
                 p.setTextFill(Color.BLACK);
                 p.setFont(Font.font("Cooper Black",25));
                 winnerPane.getChildren().addAll(p);
+                winnerPane.setBackground(background);
 
             } else if (won == false){
                 System.out.println("player 2 won");
                 p.setText("player 2 won");
-                p.setTranslateX(50);
-                p.setTranslateY(250);
+                p.setTranslateX(205);
+                p.setTranslateY(590);
                 p.setTextFill(Color.BLACK);
                 p.setFont(Font.font("Cooper Black",25));
                 winnerPane.getChildren().addAll(p);
